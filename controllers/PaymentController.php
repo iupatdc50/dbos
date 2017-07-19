e21ae49d60b3fdb82db9e5f32c00a47c182bba57
J. M. De Moor <josephmdemoor@programmer.net>
2017-07-18 20:44:26 -0700

    Bug fix
    AccessControl class not in uses clause for Payment controller

--- controllers/PaymentController.php
+++ controllers/PaymentController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use Yii;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
+use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
 use app\models\project\jtp\Payment;
 use app\models\project\jtp\Project;
