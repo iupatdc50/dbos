7143677ec89781ec376d3a50379b90e97dd98715
J. M. De Moor <josephmdemoor@programmer.net>
2019-07-30 12:14:08 -0700

    Update SummaryController.php
    Specified JSON response format

--- controllers/base/SummaryController.php
+++ controllers/base/SummaryController.php
@@ -2,13 +2,11 @@
 
 namespace app\controllers\base;
 
-use app\controllers\base\SubmodelController;
 use Yii;
 use yii\data\ActiveDataProvider;
-use yii\web\Controller;
-use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
+use yii\db\ActiveQuery;
 use yii\helpers\Json;
+use Yii\web\Response;
 
 /**
  * Standard extension to process JSON calls
@@ -32,20 +30,26 @@ class SummaryController extends SubmodelController
 	
 	public function actionSummaryJson($id)
 	{
-		$query = call_user_func([$this->recordClass, 'find'])
+        /** @var ActiveQuery $query */
+        $query = call_user_func([$this->recordClass, 'find'])
 					->where([$this->relationAttribute => $id])
 					->orderBy($this->summOrder);
+
 		if (!empty($this->summJoinWith))
 			$query->joinWith($this->summJoinWith);
+
 		foreach ($this->summWhere as $col => $val)
 			$query->andWhere([$col => $val]);
+
 		$dataProvider = new ActiveDataProvider([
 				'query' => $query,
 				'pagination' => ['pageSize' => $this->summPageSize],
 				'sort' => false,
 		]);
+
 		$params = array_merge(['dataProvider' => $dataProvider, 'id' => $id], $this->viewParams);
-//		echo Json::encode($this->renderPartial('_summary', $params));
+
+        Yii::$app->response->format = Response::FORMAT_JSON;
         /** @noinspection MissedViewInspection */
         echo Json::encode($this->renderAjax('_summary', $params));
 	}
