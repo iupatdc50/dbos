127953bd8926b81ca42156a5431a5f1ba7c282ef
J. M. De Moor <jmdemoor@objectpac.com>
2020-01-13 19:00:25 -0800

    Complete interface for Init Fee updates

--- modules/admin/views/dues-rate/_search.php
+++ modules/admin/views/dues-rate/_search.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\value\BillRateSearch */
+/* @var $model app\models\accounting\DuesRateSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
